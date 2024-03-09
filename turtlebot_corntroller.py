637bf94dc81e15e672cf2ba8299362afe642eff3
Bwodetzki <50225476+Bwodetzki@users.noreply.github.com>
2024-03-09 00:49:38 -0600

    designed corntroller for turtlebot

---
+++ turtlebot_corntroller.py
@@ -0,0 +1,131 @@
+import pybullet as p
+import numpy as np
+import time
+
+def angle_diff(theta1, theta2):
+    diff = theta2 - theta1
+    return np.arctan2(np.sin(diff), np.cos(diff))
+
+def corntroller_v1(target_pos, turtlebot_id, max_vel=10, eps=1e-1):
+    ''' 
+    Velocity controller for turtlebot.
+
+    Inputs:
+    target_pos: Target Position coordinate
+        np.array of length 3
+
+    turtlebot_id: Id of turtlebot object
+        idk
+    
+    Main Issues:
+    2 modes: turning and drive straight. Not very natural. (when eps is small, the turtlebot will overshoot the turning and have jittery behavior)
+    '''
+
+    # Note that orn = orientation
+    curr_pos, curr_orn = p.getBasePositionAndOrientation(turtlebot_id)
+    curr_orn = p.getEulerFromQuaternion(curr_orn)
+
+    # Lets move to 2d
+    curr_orn = np.array(curr_orn[-1])
+    curr_pos = np.array(curr_pos[0:2])
+    target_pos = np.array(target_pos[0:2])
+
+    desired_traj = target_pos - curr_pos
+    desired_orn = np.arctan2(desired_traj[1], desired_traj[0])
+
+    # Check if at target
+    if np.linalg.norm(desired_traj) < eps:
+        return (0, 0)
+
+    # Calculate Wheel Velocities
+    leftWheelVelocity=0
+    rightWheelVelocity=0
+
+    orn_error = angle_diff(curr_orn, desired_orn)
+    if abs(orn_error)>eps:
+        leftWheelVelocity=-1*max_vel*np.sign(orn_error)
+        rightWheelVelocity=-1*leftWheelVelocity
+    else:
+        leftWheelVelocity=max_vel
+        rightWheelVelocity=max_vel
+    
+    return (leftWheelVelocity, rightWheelVelocity)
+
+
+def corntroller_v2(target_pos, turtlebot_id, a=5, max_vel=10, eps=1e-2):
+    ''' 
+    Velocity controller for turtlebot with more natural movement.
+    Key idea: instead of a hard cutoff at epsilon when turning and going straight, one should use a smooth function to transtition between the two. An exponential is a reasonable choice. 
+
+    Notes that "a" is not well tuned right now
+    The higher the "a", the faster it will want to turn (above 5-10 the difference is minimal)
+
+    The lower the "a" the more it will want to go forward
+
+    Maximum velocity should be determined on a wheel by wheel basis (since at least in real life, wheel slippage is the limmiting factor)
+    Some velocity normalization would solve this.
+
+    Inputs:
+    target_pos: Target Position coordinate
+        np.array of length 3
+
+    turtlebot_id: Id of turtlebot object
+        idk
+    '''
+
+    func = lambda x: np.exp(-a*x)
+    # Note that orn = orientation
+    curr_pos, curr_orn = p.getBasePositionAndOrientation(turtlebot_id)
+    curr_orn = p.getEulerFromQuaternion(curr_orn)
+
+    # Lets move to 2d
+    curr_orn = np.array(curr_orn[-1])
+    curr_pos = np.array(curr_pos[0:2])
+    target_pos = np.array(target_pos[0:2])
+
+    desired_traj = target_pos - curr_pos
+    desired_orn = np.arctan2(desired_traj[1], desired_traj[0])
+    # desired_orn = np.arctan(desired_traj[1]/desired_traj[0])
+
+
+    # Check if at target
+    if np.linalg.norm(desired_traj) < eps:
+        return (0, 0)
+
+    # Calculate Wheel Velocities
+    leftWheelVelocity=0
+    rightWheelVelocity=0
+
+    orn_error = angle_diff(curr_orn, desired_orn)
+    
+    leftWheelVelocity = max_vel*(func(abs(orn_error)) - (1 - func(abs(orn_error)))*np.sign(orn_error))
+    rightWheelVelocity = max_vel*(func(abs(orn_error)) + (1 - func(abs(orn_error)))*np.sign(orn_error))
+    
+    return (leftWheelVelocity, rightWheelVelocity)
+
+
+if __name__=='__main__':
+    p.connect(p.GUI)
+    turtle_offset = [0,0,0]
+
+    turtle = p.loadURDF("turtlebot.urdf",turtle_offset)
+    plane = p.loadURDF("plane.urdf")
+    p.setRealTimeSimulation(1)
+
+    p.setGravity(0,0,-10)
+
+    target = np.array([-3, 1, 0])  # Set the target for the turtlebot here
+
+    while (1):
+        time.sleep(1./240.)
+        keys = p.getKeyboardEvents()
+        leftWheelVelocity=0
+        rightWheelVelocity=0
+        speed=10
+
+        # Find Wheel Velocities
+        leftWheelVelocity, rightWheelVelocity = corntroller_v2(target, turtle, max_vel=speed)
+
+        # Update Turtle Bot
+        p.setJointMotorControl2(turtle,0,p.VELOCITY_CONTROL,targetVelocity=leftWheelVelocity,force=1000)
+        p.setJointMotorControl2(turtle,1,p.VELOCITY_CONTROL,targetVelocity=rightWheelVelocity,force=1000)
