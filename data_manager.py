e31db3abf1f04670e6eb6bcf655c81c355122f6c
Zach Cawood <zcawood@purdue.edu>
2024-04-01 23:35:02 -0400

    added TrainingPoint dataclass
    -frozen makes the class attributes read-only
    -slots makes the class use less memory and access its attributes faster

---
+++ data_manager.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+import numpy as np
+
+@dataclass(frozen=True, slots=True) # frozen makes it read-only/immutable, slots makes it use less memory and makes accessing faster
+class TrainingPoint():
+    currPosition: np.array # current position of the bot
+    lidarMeasurements: list[float] # lidar measurement array at the current position
+    targetPosition: np.array # desired next position along the bot's path
+    endPosition: np.array # terminal position on the bot's path
